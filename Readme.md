13401d6883a30d721557d62abfbec8653f83759b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to show/hide a custom button in an editor
+
+
+<p><em><strong>Update:<br /></strong>Starting with version 10.2.10, you can use the ButtonInfo.Visibility property to hide/show a custom button.</em><br /><br />This sample illustrates how to show/hide a custom button in the ComboBoxEdit. Inherit from the ButtonInfo, and implement the ButtonInfoVisibility dependency property. Implement the ButtonInfo template, and bind the Button.Visibility property to the ButtonInfoVisibility property. We plan to implement this property: <a href="https://www.devexpress.com/Support/Center/p/S35955">S35955</a></p>
+
+<br/>
+
+
---
+++ VB/WpfApplication17/MainWindow.xaml.vb
@@ -0,0 +1,54 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Editors
+
+Namespace WpfApplication17
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim bi As MyButtonInfo = TryCast(comboBoxEdit1.Buttons(0), MyButtonInfo)
+			If bi.ButtonInfoVisibility = System.Windows.Visibility.Visible Then
+				bi.ButtonInfoVisibility = System.Windows.Visibility.Hidden
+			Else
+				bi.ButtonInfoVisibility = System.Windows.Visibility.Visible
+			End If
+		End Sub
+	End Class
+
+
+	Public Class MyButtonInfo
+		Inherits ButtonInfo
+		Public Property ButtonInfoVisibility() As Visibility
+			Get
+				Return CType(GetValue(ButtonInfoVisibilityProperty), Visibility)
+			End Get
+			Set(ByVal value As Visibility)
+				SetValue(ButtonInfoVisibilityProperty, value)
+			End Set
+		End Property
+
+		' Using a DependencyProperty as the backing store for ButtonInfoVisibility.  This enables animation, styling, binding, etc...
+		Public Shared ReadOnly ButtonInfoVisibilityProperty As DependencyProperty = DependencyProperty.Register("ButtonInfoVisibility", GetType(Visibility), GetType(MyButtonInfo), New UIPropertyMetadata(Visibility.Visible))
+
+
+	End Class
+End Namespace
